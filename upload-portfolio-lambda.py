357ffbd0d1d5cd9464b0ab2542cbcb6e0d3d4fb1
LiaVinci <liav.avnaim72@gmail.com>
2018-08-11 22:32:07 +0200

    Updating our deploy script
    Now everytime we will deploy our portfolio we will recieve an email using an sns topic.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,18 +3,29 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-central-1:685862621175:deployPortfolioTopic')
 
-portfolio_bucket = s3.Bucket('portfolio.liavinci.com')
-build_bucket = s3.Bucket('portfoliobuild.liavinci.com')
+    try:
+        s3 = boto3.resource('s3')
 
-# Instead of working with the file system we are using StringIO to be able to use the file from memory
-portfolio_zip = StringIO.StringIO()
+        portfolio_bucket = s3.Bucket('portfolio.liavinci.com')
+        build_bucket = s3.Bucket('portfoliobuild.liavinci.com')
 
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        # Instead of working with the file system we are using StringIO to be able to use the file from memory
+        portfolio_zip = StringIO.StringIO()
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for fileName in myzip.namelist():
-        obj = myzip.open(fileName)
-        portfolio_bucket.upload_fileobj(obj, fileName, ExtraArgs={'ContentType': mimetypes.guess_type(fileName)[0]})
-        portfolio_bucket.Object(fileName).Acl().put(ACL='public-read')
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for fileName in myzip.namelist():
+                obj = myzip.open(fileName)
+                portfolio_bucket.upload_fileobj(obj, fileName, ExtraArgs={'ContentType': mimetypes.guess_type(fileName)[0]})
+                portfolio_bucket.Object(fileName).Acl().put(ACL='public-read')
+
+        topic.publish(Subject="Portfolio Deployed", Message="Your Portfolio Was Deployed Succcessfully!")
+
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="Your Portfolio Has Failed : ( Please Check Your AWS Console.")
+        raise
