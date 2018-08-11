2bb7d2a644a07b8b41405bac456828151baca372
LiaVinci <liav.avnaim72@gmail.com>
2018-08-11 23:00:23 +0200

    Updating our deployment script
    Now our lambda python script can run manually as well as by CodePipeline.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,16 +7,31 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:eu-central-1:685862621175:deployPortfolioTopic')
 
+    # Setting location with default values so we can run this function manually as well as inside CodePipeline
+    location = {
+        "bucketName": 'portfoliobuild.liavinci.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
+
     try:
+        job = event.get('CodePipeline.job')
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(location)
+
         s3 = boto3.resource('s3')
 
         portfolio_bucket = s3.Bucket('portfolio.liavinci.com')
-        build_bucket = s3.Bucket('portfoliobuild.liavinci.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         # Instead of working with the file system we are using StringIO to be able to use the file from memory
         portfolio_zip = StringIO.StringIO()
 
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for fileName in myzip.namelist():
@@ -25,6 +40,11 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(fileName).Acl().put(ACL='public-read')
 
         topic.publish(Subject="Portfolio Deployed", Message="Your Portfolio Was Deployed Succcessfully!")
+
+        # If we ran this function from CodePipeline we are using this if statement to tell CodePipeline that the Job ran successfully
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
 
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="Your Portfolio Has Failed : ( Please Check Your AWS Console.")
